f0f4f52a74016e52de9930ddbdc99bc4bc9ce12d
RashaunB64 <54905696+RashaunB64@users.noreply.github.com>
2019-09-11 12:30:04 -0400

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Rashaun Buchanan
+ *
+ */
+public class Test1 {
+	public static void main(String[] args) {
+//		System.out.println(78+98); //176
+//		System.out.println('a'+'b'); //195
+//		System.out.println('a'-'b'); //-1
+//		System.out.println('0'+'0'); //96
+//		System.out.println('a'+1); //98
+//		System.out.println('a'); //a
+		
+		int num = 4;
+		double dollars = 23.50;
+		
+		System.out.printf(" I have %d cars %n", num); //I have 10 cars
+		System.out.printf(" Maria has $%.2f in her pocket", dollars);
+		
+		System.out.printf("%n %s is %d years older than %s", "Justin", num, "Tim");
+	}
+
+}

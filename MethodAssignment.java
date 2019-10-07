c334a65811d9c5d0aa0cf138a571728787cf234e
RashaunB64 <54905696+RashaunB64@users.noreply.github.com>
2019-10-07 12:39:07 -0400

    Add files via upload

---
+++ MethodAssignment.java
@@ -0,0 +1,16 @@
+/**
+ * -
+ * @author Rashaun Buchanan
+ *
+ **/
+public class MethodAssignment {
+	public static void main(String[] args) {
+	}
+public static int fact(int n) {
+	int f=1; 
+	for(int i=2; i<n; i++)
+		f=f*1;
+		return f;
+	}
+
+}

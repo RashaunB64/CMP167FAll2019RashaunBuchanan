ac25b0c771e730918e7ff00bfa006605d34adfb3
RashaunB64 <54905696+RashaunB64@users.noreply.github.com>
2019-10-21 12:07:38 -0400

    Add files via upload

---
+++ Practice_Answers.java
@@ -0,0 +1,19 @@
+
+	import java.util.Scanner;
+	public class Practice_Answers {
+	   public static void main(String[] args) {
+	      Scanner scnr = new Scanner(System.in);
+
+	      System.out.println("Enter number of Miles :"); 
+	     double Miles;
+	     double KM;
+	      KM = 0.62;
+	      Miles = scnr.nextInt();
+	     double result1 = Miles * KM;
+	      System.out.println(Miles+" miles is equal to "+result1+" km."  );
+	      
+	      
+	   }
+	}
+	
+
